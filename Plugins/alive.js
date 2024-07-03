079c8cd1a3c644fd8c1e165b89535d563a5e890e
Saban_AI <129470870+BAR9Shadow@users.noreply.github.com>
2024-07-03 10:52:57 +0100

    Create alive.js

---
+++ Plugins/alive.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+╭────────────────────
+│👋 مرحبا يا , ${conn.getName(m.sender)}!
+│🤖 أتمنى أنك بخير ♥ البوت أون لاين الآن 
+ |حالة البوت : في تطوير 😇📂
+يمكنك إستخدامه عبر كتابة menu.
+
+╰────────────────────
+*─[ BY SABAN BOT ]*🌟✨
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['alive']
+handler.tags = ['infobot']
+handler.command = /^(alive)$/i
+
+
+export default handler
